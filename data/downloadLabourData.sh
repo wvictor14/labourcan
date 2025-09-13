8213b8cccc17a42b2bf88aa201ded58b511e32a9
Victor Yuan <victor.2wy@gmail.com>
2025-09-13 05:45:00 +0000

    add -O

--- data/downloadLabourData.sh
+++ data/downloadLabourData.sh
@@ -19,6 +19,6 @@ cd "$TARGET_DIR"
 echo "Downloading to: $(pwd)"
 URL=$(curl -s 'https://www150.statcan.gc.ca/t1/wds/rest/getFullTableDownloadCSV/14100355/en' | jq -r '.object')
 curl -o 14100355-eng.zip "$URL"
-unzip 14100355-eng.zip
+unzip -o 14100355-eng.zip
 rm 14100355-eng.zip
 echo "Download completed in: $TARGET_DIR"
